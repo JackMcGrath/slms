a387e63211f50a035142b19e436070955a5e9dc4
Alexander Shevchuk <shevchukallex@gmail.com>
2012-12-13 16:10:50 +0200

    #19 Course members page

--- src/Zerebral/FrontendBundle/Controller/CourseController.php
+++ src/Zerebral/FrontendBundle/Controller/CourseController.php
@@ -108,4 +108,20 @@ class CourseController extends \Zerebral\CommonBundle\Component\Controller
             'target' => 'courses'
         );
     }
+
+    /**
+     * @Route("/members/{id}", name="course_members")
+     * @PreAuthorize("hasRole('ROLE_STUDENT') or hasRole('ROLE_TEACHER')")
+     * @ParamConverter("course")
+     * @Template()
+     */
+    public function membersAction(Model\Course\Course $course)
+    {
+        return array(
+            'students' => $course->getStudents(),
+            'teachers' => $course->getTeachers(),
+            'course' => $course,
+            'target' => 'members'
+        );
+    }
 }
