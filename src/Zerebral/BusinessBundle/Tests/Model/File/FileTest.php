8be8c7eeae7d5acb7e4a613f4b9c9ee893064703
Dmitry Ovchinnikov <dmitry.ovchinnikov@weavora.com>
2012-12-14 19:44:13 +0300

    #7. Adding model "File" with tests

--- src/Zerebral/BusinessBundle/Tests/Model/File/FileTest.php
+++ src/Zerebral/BusinessBundle/Tests/Model/File/FileTest.php
@@ -10,21 +10,16 @@ class FileTest extends \Zerebral\BusinessBundle\Tests\TestCase {
     public function testFileCreation() {
         $file = new File();
 
-        $file->setName('test.txt');
-        $file->setSize(21038);
-        $file->setMimeType('text');
-        $file->setCreatedAt(new \DateTime());
+        $file->setSourcePath('/home/deespater/Sandbox/uploadFile/File.php');
+        //$file->setFileStorage(\Zerebral\CommonBundle\Component\FileStorage\AbstractFileStorage::getFileStorage('local'));
 
 
-
-        $assignmentQuery = new AssignmentQuery();
-        $assignment = $assignmentQuery->findOne();
-
-        $file->addAssignment($assignment);
+//        $assignmentQuery = new AssignmentQuery();
+//        $assignment = $assignmentQuery->findOne();
+//        $file->addAssignment($assignment);
 
         $file->save();
 
-
-        $file->delete();
+        //$file->delete();
     }
 }
