1f80c070d3c7f86b703d91cad1a5428d7df1a9b7
Slava Belokurski <slavchoo2@gmail.com>
2012-12-15 13:54:04 +0300

    #30 Course. Description

--- src/Zerebral/FrontendBundle/Resources/public/js/controllers/course/add.js
+++ src/Zerebral/FrontendBundle/Resources/public/js/controllers/course/add.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-    $('.show-input').on('click', showInput);
-    $('.show-dropdown').on('click', showDropdown);
     $('textarea').wysihtml5();
     $('.optional-model').optionalModel();
 });
