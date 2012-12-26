34c8c28dcdcf14048c7be696c281e0dd1b7e7371
Dmitry Ovchinnikov <dmitry.ovchinnikov@weavora.com>
2012-12-26 14:08:20 +0300

    #47. Adding UI

--- src/Zerebral/FrontendBundle/Resources/public/js/controllers/assignments/view.js
+++ src/Zerebral/FrontendBundle/Resources/public/js/controllers/assignments/view.js
@@ -4,4 +4,25 @@ $(document).ready(function(){
     },function(e) {
         $(this).find('.manage-buttons').css('visibility', 'hidden').prev().show();
     });
+
+
+    $('#uploadModal').on('show', function(e) {
+        $(this).find('ul li').remove();
+        $('#addUploadFile').click();
+    });
+
+    $('#addUploadFile').click(function(e) {
+        e.preventDefault();
+
+        var ul = $(e.target).parent();
+        var liCount = ul.find('li').length;
+        var newLi = $('<li><input type="file" name="solution[file][' + liCount + ']" /><input type="text" placeholder="Description (optional)" /></li>');
+        if (liCount == 0) {
+            ul.prepend(newLi);
+        } else {
+            $(e.target).prev().append(newLi);
+        }
+
+    });
+
 });
