e81f345861c2a8b71ad6c7696777c42e7520bd9c
yury.tolochko <yury.tolochko@weavora.com>
2013-01-04 18:00:29 +0300

    #59 Replace manual ACL to voter in Uploading Solutions

---
+++ src/Zerebral/CommonBundle/Validator/Constraints/DateCompare.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Zerebral\CommonBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Exception\ConstraintDefinitionException;
+
+/**
+ * {@inheritDoc}
+ */
+class DateCompare extends Compare
+{
+
+}
