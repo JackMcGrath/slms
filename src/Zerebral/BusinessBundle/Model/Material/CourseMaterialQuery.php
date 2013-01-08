c0c5ddea2944672d55615ca8788da59046d78267
Alexander Shevchuk <shevchukallex@gmail.com>
2013-01-08 15:52:15 +0200

    #86 Recent materials widget

--- src/Zerebral/BusinessBundle/Model/Material/CourseMaterialQuery.php
+++ src/Zerebral/BusinessBundle/Model/Material/CourseMaterialQuery.php
@@ -6,4 +6,11 @@ use Zerebral\BusinessBundle\Model\Material\om\BaseCourseMaterialQuery;
 
 class CourseMaterialQuery extends BaseCourseMaterialQuery
 {
+    public function findRecentCourseMaterials($course, $limit = 5)
+    {
+        $this->setLimit($limit);
+        $this->addDescendingOrderByColumn('created_at');
+        $this->filterByCourseId($course->getId());
+        return $this;
+    }
 }
