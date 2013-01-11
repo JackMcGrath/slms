314ad9903a710edcdc647b098bde4f674503e9e7
Alexander Shevchuk <shevchukallex@gmail.com>
2013-01-11 12:27:50 +0300

    #67 Fixed problem with edit attendance

--- src/Zerebral/FrontendBundle/Resources/public/js/controllers/attendance/index.js
+++ src/Zerebral/FrontendBundle/Resources/public/js/controllers/attendance/index.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
     $('.pick-date').datepicker().on('changeDate', function(e) {
-        location.href = '?date=' + (e.date.getTime() + (e.date.getTimezoneOffset() * 60000))/ 1000;
+        location.href = '?date=' + (e.date.getTime() - (e.date.getTimezoneOffset() * 60000))/ 1000;
     });
 
     $('th.status input[type="radio"]').change(function(e) {
         $('td input[type="radio"]').removeAttr('checked');
         $('td[statusName="'+$(e.target).val()+'"] input[type="radio"]').attr('checked', 'checked');
+
+        $('th.status input[type="radio"]').removeAttr('checked');
+        $(e.target).attr('checked', 'checked');
     });
 
     $('td.status input[type="radio"]').change(function(e) {
