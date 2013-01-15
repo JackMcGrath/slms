f68dd999e8d4efce5fe58dcc72bd1963628f9ed2
Alexander Shevchuk <shevchukallex@gmail.com>
2013-01-15 13:24:29 +0300

    #65 Style changes. Show correct notification for attendance

--- src/Zerebral/BusinessBundle/Model/Notification/Notification.php
+++ src/Zerebral/BusinessBundle/Model/Notification/Notification.php
@@ -11,4 +11,31 @@ class Notification extends BaseNotification
         $this->setCreatedAt(date("Y-m-d H:i:s", time()));
         return parent::preInsert($con);
     }
+
+    public function getParams()
+    {
+        if (empty($this->params)) {
+            return array();
+        }
+
+        return json_decode($this->params, true);
+    }
+
+    public function setParams($params)
+    {
+        parent::setParams(json_encode($params));
+    }
+
+    public function setParam($name, $value)
+    {
+        $params = $this->getParams();
+        $params[$name] = $value;
+        $this->setParams($params);
+    }
+
+    public function getParam($name)
+    {
+        $params = $this->getParams();
+        return isset($params[$name]) ? $params[$name] : null;
+    }
 }
