8644825e6549ef1a5b8c13b45a81ae0bb08ead64
Dmitry Ovchinnikov <dmitry.ovchinnikov@weavora.com>
2013-02-01 13:26:12 +0300

    #154. Partial for child selection is done

--- src/Zerebral/BusinessBundle/Model/User/Guardian.php
+++ src/Zerebral/BusinessBundle/Model/User/Guardian.php
@@ -6,4 +6,19 @@ use Zerebral\BusinessBundle\Model\User\om\BaseGuardian;
 
 class Guardian extends BaseGuardian
 {
+    public function getSelectedChild($childId = null) {
+        $criteria = null;
+
+        if (!is_null($childId)) {
+            $criteria = new \Criteria();
+            $criteria->add(StudentPeer::ID, $childId, \Criteria::EQUAL);
+        }
+
+        return $this->getStudents($criteria)->getFirst();
+    }
+
+    public function isGuardianFor(Student $student)
+    {
+        return !(is_null($this->getSelectedChild($student->getId())));
+    }
 }
--- src/Zerebral/FrontendBundle/Controller/GuardianController.php
+++ src/Zerebral/FrontendBundle/Controller/GuardianController.php
@@ -4,7 +4,12 @@ namespace Zerebral\FrontendBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use JMS\SecurityExtraBundle\Annotation\Secure;
+use JMS\SecurityExtraBundle\Annotation\SecureParam;
+use JMS\SecurityExtraBundle\Annotation\PreAuthorize;
+
+use Zerebral\BusinessBundle\Model\User\Student;
 
 /**
   * @Route("/parent-area")
@@ -18,7 +23,29 @@ class GuardianController extends \Zerebral\CommonBundle\Component\Controller
      */
     public function indexAction()
     {
-        return array();
+        return array(
+            'target' => 'home'
+        );
+    }
+
+    /**
+     * @Route("/set-child/{childId}", name="guardian_set_child")
+     * @Secure(roles="ROLE_GUARDIAN")
+     * @param \Zerebral\BusinessBundle\Model\User\Student $student
+     * @throws \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @ParamConverter("student", options={"mapping": {"childId": "id"}})
+     */
+    public function setSelectedChild(\Zerebral\BusinessBundle\Model\User\Student $student)
+    {
+        $referrer = $this->getRequest()->headers->get('referer');
+        if (($this->getRoleUser()->isGuardianFor($student)) && (!is_null($referrer))) {
+            $this->get('session')->set('selectedChildId', $student->getId());
+            return $this->redirect($this->getRequest()->headers->get('referer'));
+        } else {
+            throw new \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException('You can\'t access this URL directly');
+        }
+
     }
 
     /**
@@ -38,8 +65,13 @@ class GuardianController extends \Zerebral\CommonBundle\Component\Controller
      */
     public function assignmentsAction()
     {
+        /** @var \Zerebral\BusinessBundle\Model\User\Guardian $guardian  */
+        $guardian = $this->getRoleUser();
+        $child = $guardian->getSelectedChild($this->get('session')->get('selectedChildId'));
         return array(
-            'guardian' => $this->getRoleUser()
+            'target' => 'home',
+            'guardian' => $guardian,
+            'child' => $child
         );
     }
 }
